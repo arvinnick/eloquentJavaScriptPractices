abb90b1d6500206e8e3592695acf01ba7c87b199
arvinnick <arvin.nick.90@gmail.com>
2025-09-13 23:40:42 +0400

    chapter 4 exercise 2

--- chapter4.js
+++ chapter4.js
@@ -32,4 +32,84 @@ function arrSum(arr){
     return retSum;
 }
 
-console.log(rangeFunc(0, 5, -2));
+/*
+Reversing an array
+Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this
+exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, should take an array as
+its argument and produce a new array that has the same elements in the inverse order. The second, reverseArrayInPlace,
+should do what the reverse method does: modify the array given as its argument by reversing its elements. Neither may
+use the standard reverse method.
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect
+to be useful in more situations? Which one runs faster?
+ */
+function reverseArray(arr){
+    let retArr = [];
+    for(let i = arr.length - 1; i>=0; i--){
+        retArr.push(arr[i]);
+    }
+    return retArr
+}
+
+function reverseArrayInPLace(arr){
+    for(let i = 0; i<=arr.length - 1; i++){
+        arr.push(arr[i])
+        arr.unshift(arr[i])
+    }
+}
+/*
+A list
+As generic blobs of values, objects can be used to build all sorts of data structures. A common data structure is the
+list (not to be confused with arrays). A list is a nested set of objects, with the first object holding a reference to
+the second, the second to the third, and so on:
+let list = {
+    value: 1,
+    rest: {
+        value: 2,
+        rest: {
+                value: 3,
+                rest: null
+            }
+        }
+};
+The resulting objects form a chain, as shown in the following diagram:
+A nice thing about lists is that they can share parts of their structure. For example, if I create two new values
+{value: 0, rest: list} and {value: -1, rest: list} (with list referring to the binding defined earlier), they are both
+independent lists, but they share the structure that makes up their last three elements. The original list is also still
+a valid three-element list. Write a function arrayToList that builds up a list structure like the one shown when given
+[1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Add the helper functions
+prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list,
+and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring
+to the first element) or undefined when there is no such element. If you haven’t already, also write a recursive version
+of nth.
+ */
+
+function linker(element, other_element){
+    element.rest = other_element;
+    return element
+}
+function nth(index, list, passed_elements=0){
+    let ret = list;
+    if(!ret || passed_elements===index){
+        return ret;
+    }
+    else{
+        return nth(index, list, passed_elements + 1);
+    }
+}
+function arrayToList(arr){
+    if(arr.length === 1){
+        return {
+            value:arr[0],
+            rest:null
+        };
+    } else{
+     return linker({
+         value: arr[0]
+     },
+         arrayToList(arr.slice(1)));
+    }
+}
+
+console.log(arrayToList([0,1,2]));
+
+
