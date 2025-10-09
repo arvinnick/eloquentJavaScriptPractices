984bdaf34c73365c123dd5bbedf3cb33269f23cb
arvinnick <arvin.nick.90@gmail.com>
2025-10-10 00:07:45 +0400

    chapter 4 exercise 3

--- chapter5.js
+++ chapter5.js
@@ -8,8 +8,8 @@ function flatten(array) {
 }
 
 
-console.log(flatten([[1,2,3],[4,5,6]]))
-console.log(flatten([[[1,2,3],[4,5,6]], [[7,8,9],[10,11,12]]]))
+// console.log(flatten([[1,2,3],[4,5,6]]))
+// console.log(flatten([[[1,2,3],[4,5,6]], [[7,8,9],[10,11,12]]]))
 
 
 
@@ -21,3 +21,24 @@ loop value and stop if that returns false. It should then call the body function
 finally call the update function to create a new value and start over from the beginning. When defining the function,
 you can use a regular loop to do the actual looping.
  */
+
+
+
+/*
+Everything
+Arrays also have an every method analogous to the some method. This method returns true when the given function returns
+true for every element in the array. In a way, some is a version of the || operator that acts on arrays, and every is
+like the && operator. Implement every as a function that takes an array and a predicate function as parameters. Write
+two versions, one using a loop and one 142using the some method.
+ */
+
+function loopEverything(array, predicateFunction){
+    let every = true;
+    array.reduce(predicateFunction, every);
+    /*array.forEach(x=>{every && predicateFunction(x)})*/
+    return every;
+}
+
+let arr = [0, 1, 2, 3];
+const prFunc = x=> x<4;
+console.log(loopEverything(arr, prFunc));
