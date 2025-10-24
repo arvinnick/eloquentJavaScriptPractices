3d0b466d172a8c2bb211fdf1e7082ac13b0937e6
arvinnick <arvin.nick.90@gmail.com>
2025-10-24 22:45:02 +0400

    chapter 8 exercise 2

--- ch8.js
+++ ch8.js
@@ -19,3 +19,45 @@ function primitiveMultiplyWrapper(primitiveMultiply){
     }
     return ret;
 }
+
+/*
+The locked box
+Consider the following (rather contrived) object:
+ */
+const box = new class {
+    locked = true;
+    #content = [];
+
+    unlock() {
+        this.locked = false;
+    }
+
+    lock() {
+        this.locked = true;
+    }
+
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this.#content;
+    }
+};
+/*
+It is a box with a lock. There is an array in the box, but you can get at it only when the box is unlocked.
+Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, runs the function, and
+then ensures that the box is locked again before returning, regardless of whether the argument function returned
+normally or threw an exception.
+For extra points, make sure that if you call withBoxUnlocked when the box is already unlocked, the box stays unlocked.
+ */
+
+function withBoxUnlocked(callable){
+    let lockAtTheEnd;
+    lockAtTheEnd = box.locked;
+    box.unlock();
+    try {
+        callable();
+    } catch (e) {throw e} finally {
+        if (lockAtTheEnd){
+            box.lock();
+        }
+    }
+}
